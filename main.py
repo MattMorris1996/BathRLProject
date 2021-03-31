a787b829a969c5e74c39f8f90d673be6ad7d9f3c
Matthew Morris <matthew.m1996@gmail.com>
2021-03-31 14:01:12 +0100

    added the main and the env

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import gym
+
+
 def main():
+    env = gym.make('CarRacing-v0')
+    env.reset()
+    for _ in range(1000):
+        env.render()
+        env.step(env.action_space.sample())  # take a random action
+    env.close()
     pass
 
 
