d3f628599985f460bfef01c77132e366b5a15e39
tburde <appa1962>
2021-03-31 13:40:29 +0100

    Added imports for envs

--- Tests/MattTest.py
+++ Tests/MattTest.py
@@ -5,3 +5,22 @@
 #Hello Tanmay
 
 #Hello World
+
+import sys
+import math
+import numpy as np
+
+import Box2D
+from Box2D.b2 import fixtureDef
+from Box2D.b2 import polygonShape
+from Box2D.b2 import contactListener
+
+import gym
+from gym import spaces
+from gym.envs.box2d.car_dynamics import Car
+from gym.utils import seeding, EzPickle
+
+import pyglet
+
+pyglet.options["debug_gl"] = False
+from pyglet import gl Box2D
