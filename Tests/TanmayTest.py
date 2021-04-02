497937d899add8d7d8d728c12179d2799f505def
tburde <appa1962>
2021-04-02 13:55:48 +0100

    minor changes + requirements.txt updated

--- Tests/TanmayTest.py
+++ Tests/TanmayTest.py
@@ -1,22 +1,21 @@
-#Let's hope this worked
-
-#Hello team!
-
-print("State modification testing")
-
 import gym
-#import time
+# import time
 import numpy as np
 import matplotlib.pyplot as plt
 
+print("State modification testing")
+
+
 def grey(array):
-    #array.transpose(2, 0, 1).reshape(3, -1)
-    greyscale = np.dot(array, [0.2989, 0.5870, 0.1140]) #[1/3, 1/3, 1/3])
+    # array.transpose(2, 0, 1).reshape(3, -1)
+    greyscale = np.dot(array, [0.2989, 0.5870, 0.1140])  # [1/3, 1/3, 1/3])
     return greyscale
 
-def crop(array, y_max = 96, y_min = 0, x_min = 0, x_max = 96):
-    array_cropped = array[y_min:y_max,x_min:x_max]
+
+def crop(array, y_max=96, y_min=0, x_min=0, x_max=96):
+    array_cropped = array[y_min:y_max, x_min:x_max]
     return array_cropped
+
 
 def main():
     env = gym.make('CarRacing-v0')
@@ -28,15 +27,15 @@ def main():
         greyscale = grey(state)
         greyscale_c = crop(greyscale, 84, 0, 24, 72)
         states = crop(greyscale, 96, 84, 12, 96)
-        #print(a)
+        # print(a)
         plt.figure(1)
         plt.imshow(greyscale_c, cmap=plt.get_cmap('gray'), vmin=0, vmax=255)
-        t = plt.show()
+        # t = plt.show()
         plt.figure(2)
         plt.imshow(states, cmap=plt.get_cmap('gray'), vmin=0, vmax=255)
-        s = plt.show()
-        #time.sleep(1/60)
-        #plt.close('all')
+        plt.show()
+        # time.sleep(1/60)
+        # plt.close('all')
     env.close()
     pass
 
