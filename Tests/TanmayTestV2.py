ef5762041cb0d17463d777953725334f62af7d34
tburde <appa1962>
2021-04-06 15:08:19 +0100

    added file for testing MCC DQN

--- Tests/TanmayTestV2.py
+++ Tests/TanmayTestV2.py
@@ -5,23 +5,40 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+# from keras.optimizers import SGD
 
 from collections import deque
 
+# import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+NUM_PARALLEL_EXEC_UNITS = 12
+
+#Assume that the number of cores per socket in the machine is denoted as NUM_PARALLEL_EXEC_UNITS
+#  when NUM_PARALLEL_EXEC_UNITS=0 the system chooses appropriate settings
+
+config = tf.ConfigProto(intra_op_parallelism_threads=NUM_PARALLEL_EXEC_UNITS,
+                        inter_op_parallelism_threads=4,
+                        allow_soft_placement=True,
+                        device_count = {'CPU': NUM_PARALLEL_EXEC_UNITS})
+
+session = tf.Session(config=config)
+
 seed = 1
 num_episodes = 1000
-max_steps = 500
+max_steps = 1500
 
 
 class Agent:
 
     epsilon = 1
-    gamma = 0.85
-    alpha = 0.01
-    tau = 1  # 0.15
+    gamma = 0.99
+    alpha = 0.0075
+    tau = 0.01
     decay = 0.995
     noise = 0
-    memory = deque(maxlen=int(1e5))
+    memory = deque(maxlen=int(1e6))
 
     def __init__(self, env, seed):
 
@@ -41,13 +58,14 @@ class Agent:
         model.add(Dense(24, activation="relu"))
         model.add(Dense(1))  # self.env.action_space.n))
         model.compile(loss="mean_squared_error", optimizer=Adam(lr=self.alpha))
+        # model.compile(loss="mean_squared_error", optimizer=SGD(lr=self.alpha))
         return model
 
     def replayBuffer(self, state, action, reward, new_state, terminal):
         self.memory.append([state, action, reward, new_state, terminal])
 
     def replay(self):
-        batch_size = 32
+        batch_size = 64
         state_update = np.zeros([batch_size, 2])
         target_update = np.zeros(batch_size)
         if len(self.memory) < batch_size:
@@ -77,9 +95,9 @@ class Agent:
 
     def train(self, state, action, reward, next_state, terminal, steps):
         self.replayBuffer(state, action, reward, next_state, terminal)
-        if steps % 25 == 0:
+        if steps % 32 == 0:
             self.replay()
-        if steps % 50 == 0:
+        if steps % 100 == 0:
             self.trainTarget()
 
     def reset(self):
@@ -104,12 +122,14 @@ def main():
     env = gym.make('MountainCarContinuous-v0')
     agent = Agent(env, seed)
     rewards = np.zeros(num_episodes)
+    rewards_100 = deque(maxlen=int(100))
+
     for episode in range(num_episodes):
         action = np.zeros(1)
         state = env.reset().reshape(1, 2)
         total_reward = 0
         for step in range(max_steps):
-            # env.render()
+            env.render()
             if step % 10 == 0:
                 # env.render()
                 pass
@@ -125,17 +145,15 @@ def main():
 
             if terminal:
                 break
-        # env.close()
+        env.close()
         # print(total_reward)
         rewards[episode] = total_reward
-        if step >= 199:
-            print("Failed to complete in episode {}".format(episode))
-            if step % 10 == 0:
-                # dqn_agent.save_model("trial-{}.model".format(trial))
-                pass
+        rewards_100.append(total_reward)
+        if np.mean(rewards_100) <= 100:  # step >= 199:
+            print("Failed to complete in episode {} with reward of {} in {} steps".format(episode, total_reward, step))
+
         else:
-            print("Completed in {} episodes, with reward of {}, average reward of {}".format(episode, total_reward, rewards.mean()))
-            # dqn_agent.save_model("success.model")
+            print("Completed in {} episodes, with reward of {}, average reward of {}".format(episode, total_reward, np.mean(rewards_100)))
             # terminal = False
             # state = env.reset().reshape(1, 2)
             # while not terminal:
@@ -145,7 +163,7 @@ def main():
             #     next_state = next_state.reshape(1, 2)
             #     state = next_state
 
-            # break
+            break
 
     # env.close()
 
