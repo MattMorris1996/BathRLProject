1b06f6c32be44ecf0b63a861564cee5766569aa5
Matthew Morris <matthew.m1996@gmail.com>
2021-05-03 20:25:53 +0100

    Added PER to new branch

--- DDPG.py
+++ DDPG.py
@@ -5,8 +5,8 @@ import tensorflow as tf
 from tensorflow import keras
 from keras.optimizers import Adam
 from Noise import Noise
-from collections import deque
 
+from PrioReplay import PrioReplay
 
 class Actor(keras.Model):
     """Attempted model"""
@@ -43,17 +43,17 @@ class DDPG:
     """The most perfect DDPG Agent you have ever seen"""
 
     # Parameters taken from various sources
-    EPSILON = 0
-    EPSILON_MIN = 0
-    DECAY_RATE = 0.9
+    epsilon = 0
+    epsilon_min = 0
+    decay_rate = 0.9
 
-    LEARN_START = 1000
-    GAMMA = 0.99
-    ALPHA = 0.002
-    TAU = 0.005
+    learn_start = 1000
+    gamma = 0.99
+    alpha = 0.002
+    tau = 0.005
 
-    mem_len = 1e5
-    memory = deque(maxlen=int(mem_len))
+    mem_len = 100000
+    pr_replay = PrioReplay(mem_len)
 
     def __init__(self, env, seed):
 
@@ -87,7 +87,7 @@ class DDPG:
                                          kernel_initializer=last_init)(hidden)
             model = Actor(inputs, outputs)
             lr_schedule = keras.optimizers.schedules.ExponentialDecay(
-                initial_learning_rate=self.ALPHA / 2,
+                initial_learning_rate=self.alpha / 2,
                 decay_steps=1e9,
                 decay_rate=1)  # This could allow us to use decaying learning rate
             model.compile(loss="huber_loss", optimizer=Adam(
@@ -104,26 +104,29 @@ class DDPG:
             hidden = keras.layers.Dense(256, activation="relu")(hidden)
             outputs = keras.layers.Dense(1, activation="linear", kernel_initializer=last_init)(hidden)
             lr_schedule = keras.optimizers.schedules.ExponentialDecay(
-                initial_learning_rate=self.ALPHA / 1,
+                initial_learning_rate=self.alpha / 1,
                 decay_steps=1e9,
                 decay_rate=1)
             model = Critic([input1, input2], outputs)
             model.compile(loss="mean_squared_error", optimizer=Adam(learning_rate=lr_schedule))  # mean_squared_error
         return model
 
-    def replay_buffer(self, state, action, reward, next_state, terminal):
-        ##TODO Implement prioritised buffer
-        self.memory.append([state, action, reward, next_state, terminal])
-
     @tf.function  # EagerExecution for speeeed
-    def replay(self, states, actions, rewards, next_states):  # , actor, target_actor, critic, target_critic):
+    def replay(self, states, actions, rewards, next_states,
+               importance_weights):  # , actor, target_actor, critic, target_critic):
         """tf function that replays sampled experience to update actor and critic networks using gradient"""
         # Very much inspired by Keras tutorial: https://keras.io/examples/rl/ddpg_pendulum/
+
         with tf.GradientTape() as tape:
             target_actions = self.target_actor(next_states, training=True)
-            q_target = rewards + self.GAMMA * self.target_critic([next_states, target_actions], training=True)
+            q_target = rewards + self.gamma * self.target_critic([next_states, target_actions], training=True)
             q_current = self.critic([states, actions], training=True)
-            critic_loss = tf.math.reduce_mean(tf.math.square(q_target - q_current))
+
+            error_sq = tf.math.square(q_target - q_current)
+
+            critic_loss = 1 / 64 * tf.math.reduce_sum(importance_weights * error_sq)
+
+            # critic_loss = tf.math.reduce_mean(tf.math.square(q_target - q_current))
 
         critic_grad = tape.gradient(critic_loss, self.critic.trainable_variables)
         self.critic.optimizer.apply_gradients(zip(critic_grad, self.critic.trainable_variables))
@@ -144,17 +147,17 @@ class DDPG:
 
     def train_target(self):
         """Standard function to update target networks by tau"""
-        self.update_weight(self.target_actor.variables, self.actor.variables, self.TAU)
-        self.update_weight(self.target_critic.variables, self.critic.variables, self.TAU)
+        self.update_weight(self.target_actor.variables, self.actor.variables, self.tau)
+        self.update_weight(self.target_critic.variables, self.critic.variables, self.tau)
 
     def sample2batch(self, batch_size=64):
         """Return a set of Tensor samples from the memory buffer of batch_size, default is 64"""
         # batch_size = 64
 
-        if len(self.memory) < batch_size:  # return nothing if not enough experiences available
+        if len(self.pr_replay.buffer) < batch_size:  # return nothing if not enough experiences available
             return
         # Generate batch and emtpy arrays
-        samples = random.sample(self.memory, batch_size)
+        samples, importance_weights = self.pr_replay.sample(batch_size, self.target_actor, self.target_critic, self.critic, self.gamma)
         next_states = np.zeros((batch_size, self.env.observation_space.shape[0]))
         states = np.zeros((batch_size, self.env.observation_space.shape[0]))
         rewards = np.zeros((batch_size, 1))
@@ -175,21 +178,20 @@ class DDPG:
         actions = tf.convert_to_tensor(actions)
         next_states = tf.convert_to_tensor(next_states)
 
-        return (states, actions, rewards, next_states)
+        return states, actions, rewards, next_states, importance_weights
 
     def train(self, state, action, reward, next_state, terminal, steps):
         """Function call to update buffer and networks at predetermined intervals"""
-        self.replay_buffer(state, action, reward, next_state, terminal)  # Add new data to buffer
-        if steps % 1 == 0 and len(self.memory) > self.LEARN_START:  # Sample every X steps
-            samples = self.sample2batch()
-            states, actions, rewards, next_states = samples
-            self.replay(states, actions, rewards, next_states)
+        self.pr_replay.add([state, action, reward, next_state, terminal])  # Add new data to buffer
+        if steps % 1 == 0 and len(self.pr_replay.buffer) > self.learn_start:  # Sample every X steps
+            states, actions, rewards, next_states, importance_weights  = self.sample2batch()
+            self.replay(states, actions, rewards, next_states, importance_weights)
         if steps % 1 == 0:  # Update targets only every X steps
             self.train_target()
 
     def reset(self):
-        self.EPSILON *= self.DECAY_RATE
-        self.EPSILON = max(self.EPSILON_MIN, self.EPSILON)
+        self.epsilon *= self.decay_rate
+        self.epsilon = max(self.epsilon_min, self.epsilon)
 
     def choose_action(self, state, scale=False):
         """Choose action based on policy and noise function. Scale option used to limit maximum actions"""
@@ -197,7 +199,7 @@ class DDPG:
         # self.epsilon = round(max(self.epsilon / 1000, self.epsilon), 5)
         # print(state[0])
         state = tf.expand_dims(tf.convert_to_tensor(state), 0)  # convert to tensor for speeeed
-        if np.random.random() < self.EPSILON:  # If using epsilon instead of exploration noise
+        if np.random.random() < self.epsilon:  # If using epsilon instead of exploration noise
             return random.uniform(-1, 1)
         if scale:
             return np.clip(0.33 * (self.env.action_space.sample()) + self.noise.sample(), -1, 1)
---
+++ PrioReplay.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+from collections import deque
+import random
+import numpy as np
+
+class PrioReplay():
+
+    def __init__(self, bufferlen):
+        print(bufferlen)
+        self.a = 0.7
+        self.b = 0.5
+
+        self.buffer = deque(maxlen=int(bufferlen))
+        self.priorities = deque(maxlen=int(bufferlen))
+
+    def add(self, exp):
+        self.buffer.append(exp)
+        self.priorities.append(max(self.priorities, default=1))
+
+    def get_importance(self, sample_probabilities):
+        importance = 1 / ((len(self.buffer) ** self.b) * (sample_probabilities ** self.b))
+        importance_weights = 1 / max(importance)
+
+        return importance_weights
+
+    def sample(self, batch_size, target_actor, target_critic, critic, discount):
+        scaled_priorities = np.array(self.priorities) ** self.a
+        sample_probabilities = scaled_priorities / sum(scaled_priorities)
+
+        indices = random.choices(range(len(self.buffer)), weights=sample_probabilities, k=batch_size)
+        samples = np.array(self.buffer)[indices]
+        samples_importance = self.get_importance(sample_probabilities[indices])
+
+        for i in indices:
+
+            [state, action, reward, next_state, terminal] = self.buffer[i]
+
+            state = tf.expand_dims(tf.convert_to_tensor(state), 0)
+            action = tf.expand_dims(tf.convert_to_tensor(action), 0)
+            next_state = tf.expand_dims(tf.convert_to_tensor(next_state), 0)
+
+            target_action = target_actor(next_state)
+            q_target = reward + discount * tf.squeeze(target_critic([next_state, target_action])).numpy()
+            q_current = tf.squeeze(critic([state, action])).numpy()
+            error = q_target-q_current
+
+            self.set_prio(error, i)
+
+        importance_tf = tf.convert_to_tensor(samples_importance)
+        importance_tf32 = tf.cast(importance_tf, dtype=tf.float32)
+
+        return samples, importance_tf32
+
+    def set_prio(self, error, indice):
+
+        self.priorities[indice] = abs(error)
